0690b9267fbcfbb46171aa8a4b966e94d5388c09
irv-codesquad <ia.manage@outlook.com>
2019-06-10 13:18:30 -0400

    Adding async/await.

--- app.js
+++ app.js
@@ -16,14 +16,24 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
 
   fetch('https://gist.githubusercontent.com/irvang/0184e891e958e4fbf498b8e9432c378e/raw/f39f3514541d402be03ce1932f330ff150aa3675/test-fetch.json')
-    .then(res => {
-      return res.json()
+    .then(fetchResponse => {
+      return fetchResponse.json()
     })
     .then(body => {
 
       // console.log(body)
       res.render('index', { data: body });
     });
+});
+
+app.get('/two', async (req, res) => {
+
+  let fetchResponse = await fetch('https://gist.githubusercontent.com/irvang/0184e891e958e4fbf498b8e9432c378e/raw/f39f3514541d402be03ce1932f330ff150aa3675/test-fetch.json')
+
+  let body = await fetchResponse.json();
+
+  console.log(body)
+  res.render('index', { data: body });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
