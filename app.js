b2cc0fba202c050b6b38e805f0b7bc0daead68fc
irvang <irvang.manage@gmail.com>
2019-06-11 14:23:30 -0400

    Commenting on async/await.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
 
   // Fetch returns a promsie. When the promise is fulfilled it triggers the
-  // .then method, which takes a callback as argument.
+  // .then method with the value returned by the promise as an argument (fetchResponse in this case).
   fetch('https://gist.githubusercontent.com/irvang/0184e891e958e4fbf498b8e9432c378e/raw/21f513c369ffcb7e0f3b202de41da4d48d8ae5e9/test-fetch.json')
     .then(fetchResponse => {
 
@@ -38,9 +38,13 @@ app.get('/', (req, res) => {
 
 app.get('/two', async (req, res) => {
 
+  // Similar to promises, here, isntead of using .then(), we "await" for the 
+  // return and store it in a variable. Note that await can only be used within
+  // a function declared as async
   let fetchResponse = await fetch('https://gist.githubusercontent.com/irvang/0184e891e958e4fbf498b8e9432c378e/raw/21f513c369ffcb7e0f3b202de41da4d48d8ae5e9/test-fetch.json')
 
-  let body = await fetchResponse.json();
+  // we "await" for the fulfillment of the promise and store it in body
+  let body = await fetchResponse.json(); 
 
   // console.log(body)
   res.render('index', { data: body });
